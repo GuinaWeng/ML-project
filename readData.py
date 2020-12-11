2cc7223f1d7dfdb143ab2d8034973abcb8de6e63
GuinaWeng <57403772+GuinaWeng@users.noreply.github.com>
2020-12-11 21:50:51 +0100

    Create readData.py

---
+++ readData.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import os
+
+tfrecords_filename = "/Users/nana/Desktop/CNN/Challenge-20201201/Challenge_train/Challenge_train/train_data.tfrecord"
+#test_data_tfrecords =
+def decoder(tfrecord_file, is_train_dataset=None):
+    dataset = tf.data.TFRecordDataset(tfrecord_file)
+    feature_discription = {
+        'image_raw': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.int64)
+    }
+ 
+    def _parse_example(example_string): 
+        feature_dic = tf.io.parse_single_example(example_string, feature_discription)
+        feature_dic['image_raw'] = tf.io.decode_jpeg(feature_dic['image_raw'])
+        return feature_dic['image_raw'], feature_dic['label']
+ 
+    batch_size = 32
+ 
+    if is_train_dataset is not None:
+        dataset = dataset.map(_parse_example).shuffle(buffer_size=2000).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+    else:
+        dataset = dataset.map(_parse_example)
+        dataset = dataset.batch(batch_size)
+ 
+    return dataset
+ 
+train_data = decoder(tfrecords_filename, 1)
+#test_data = decoder(test_tfrecord_file)
